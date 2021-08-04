22e1dbb551279744f3516b489cc0418fb32a37d3
FerranLao <ferranlao@hotmail.com>
2021-08-04 13:57:24 +0200

    cleaning data from dataframe and inserting to postgres

--- scripts/csv_to_postgress.py
+++ scripts/csv_to_postgress.py
@@ -1,5 +1,6 @@
 import os
 from dotenv import load_dotenv
+import datetime
 
 import pandas as pd
 from sqlalchemy import create_engine
@@ -10,8 +11,29 @@ engine = create_engine(postgres_url)
 
 
 df = pd.read_csv('./penguins.csv')
+
+#? this columns are all the same data Region=Anvers Stage="Adult, 1 Egg Stage"
+df.drop(["Region", "Stage"],axis=1)
+
 df.columns = [c.lower().replace(' ','_') for c in df.columns] 
-print(df['island'].unique())
-for column in df:
-   if len(df[column].value_counts()) <=1:
-       print(column)
+
+#? clean up the species name -- Adelie Penguin (Pygoscelis adeliae) -> Adelie 
+df['species']=[item.split(' ')[0] for item in df['species']]
+
+#? transform the dates from a string to a date object
+def format_date(date:str)->str:
+    splited = date.split('/')
+    splited[2]= ('20' if splited[2] else '19') + splited[2]
+    return '/'.join(splited)
+
+df['date_egg']=[datetime.datetime.strptime(format_date(date_egg), '%m/%d/%Y') for date_egg in df['date_egg']]
+
+#? remove all rows with 2 or more nuls to avoid incomplete data
+nullIndexes = []
+for index, row in df.iterrows():
+    if row.isna().sum()>2:
+        print(index)
+        nullIndexes.append(index)
+
+df = df.drop(nullIndexes)
+
--- src/config/postgress.py
+++ src/config/postgress.py
@@ -2,5 +2,7 @@
 from sqlalchemy import create_engine
 from config import POSTGRES_URL
 
+
 engine = create_engine(POSTGRES_URL)
 
+pgdb = engine.connect()
