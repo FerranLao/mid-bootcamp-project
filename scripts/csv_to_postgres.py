eda90a3df70dda56cd0b3433f94815b1fe522d00
FerranLao <ferranlao@hotmail.com>
2021-08-05 13:36:38 +0200

    data set cleaned and inserted in postgres

--- scripts/csv_to_postgres.py
+++ scripts/csv_to_postgres.py
@@ -14,12 +14,13 @@ df = pd.read_csv('./penguins.csv')
 
 #? this columns are all the same data Region=Anvers Stage="Adult, 1 Egg Stage"
 df.drop(["Region", "Stage"],axis=1)
-
 df.columns = [c.lower().replace(' ','_') for c in df.columns] 
 
 #? clean up the species name -- Adelie Penguin (Pygoscelis adeliae) -> Adelie 
 df['species']=[item.split(' ')[0] for item in df['species']]
 
+#? turn clutch_completion into boolean for simplicity
+df['clutch_completion']=[True if item == 'Yes' else False for item in df['clutch_completion']]
 #? transform the dates from a string to a date object
 def format_date(date:str)->str:
     splited = date.split('/')
@@ -32,8 +33,13 @@ df['date_egg']=[datetime.datetime.strptime(format_date(date_egg), '%m/%d/%Y') fo
 nullIndexes = []
 for index, row in df.iterrows():
     if row.isna().sum()>2:
-        print(index)
         nullIndexes.append(index)
 
 df = df.drop(nullIndexes)
 
+try:
+    df.to_sql('penguins',engine)
+    pen = engine.execute("SELECT * FROM penguins")
+    print(pen)
+except Exception as err:
+    print(err)
